4c91eb07e6f166df196f2dc0ca4be1433c1fd589
Samuel Diai <samuel@macbook-pro-de-samuel.home>
2020-03-20 12:56:51 +0100

    Fix imports

--- aging/processing/__init__.py
+++ aging/processing/__init__.py
@@ -2,5 +2,7 @@ from . import base_processing
 from . import abdominal_composition_processing
 from . import bone_composition_processing
 from . import body_composition_processing
-from . import brain_processing 
+from . import brain_processing
 from . import heart_processing
+from . import ecg_processing
+from . import anthropometry_processing
