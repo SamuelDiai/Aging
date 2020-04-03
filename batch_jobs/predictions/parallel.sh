2a9c1e9f0af3377a3fd650514a87c19cd080599c
Samuel Diai <samuel@macbook-pro-de-samuel.home>
2020-04-03 03:31:20 +0200

    Fix parallel

--- batch_jobs/predictions/parallel.sh
+++ batch_jobs/predictions/parallel.sh
@@ -3,14 +3,14 @@
 #models=( "RandomForest" )
 #datasets=( "UrineAndBlood" )
 
-targets=( "Age" "Sex")
+targets=( "Age" "Sex" )
 models=( "Xgboost" "RandomForest" "GradientBoosting" "LightGbm" "NeuralNetwork" "ElasticNet" )
-datasets=( "UrineBiochemestry" "BloodBiochemestry" "BloodCount" "Blood" "UrineAndBlood" )
+datasets=( "Anthropometry" "SpiroAndArterialAndBp" "ArterialAndBloodPressure" "ArterialStiffness" "AnthropometryBodySize" "BloodPressure" "Spirometry" )
 
 
-outer_splits=5
+outer_splits=10
 inner_splits=5
-n_iter=50
+n_iter=30
 
 memory=8G
 n_cores=4
@@ -19,12 +19,12 @@ time=30
 
 for target in "${targets[@]}"
 do
-	for model in "${models[@]}"
+	for dataset in "${datasets[@]}"
 	do
-		if [ $target != "Sex" ] || [ $model != "ElasticNet" ]
-		then
-			for dataset in "${datasets[@]}"
-			do
+		for model in "${models[@]}"
+		do
+			if [ $target != "Sex" ] || [ $model != "ElasticNet" ]
+			then
 				for ((fold=0; fold <= $outer_splits-1; fold++))
 				do
 					job_name="${target}_${model}_${dataset}_${fold}.job"
@@ -32,10 +32,9 @@ do
 					err_file="./logs/${target}_${model}_${dataset}_${fold}.err"
 					sbatch --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -t 0-11:59 batch_jobs/predictions/single.sh $model $outer_splits $inner_splits $n_iter $target $dataset $fold
 				done
-			done
-		else
-			:
-		fi
+			else
+				:
+			fi
+		done
 	done
-
 done
