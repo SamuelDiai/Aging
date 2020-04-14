32dcfe65877e6da822430d4e6f1e83abafb60826
Samuel Diai <samuel@macbook-pro-de-samuel.home>
2020-04-14 15:52:33 +0200

    Fix

--- batch_jobs/predictions/fold_specific.sh
+++ batch_jobs/predictions/fold_specific.sh
@@ -1,22 +1,17 @@
-#!/bin/bash
-#targets=( "Age" )
-#models=( "RandomForest" )
-#datasets=( "UrineAndBlood" )
-
 target="Age"
-model="Xgboost"
+model="GradientBoosting"
 #datasets=( "Anthropometry" "SpiroAndArterialAndBp" "ArterialAndBloodPressure" "ArterialStiffness" "AnthropometryBodySize" "BloodPressure" "Spirometry" )
-dataset="AnthropometryImpedance"
+dataset="Anthropometry"
 
 outer_splits=10
 inner_splits=9
 n_iter=30
-fold=9
+fold=0
 memory=8G
-n_cores=1
+n_cores=4
 
 
 job_name="${target}_${model}_${dataset}_${fold}.job"
 out_file="./logs/${target}_${model}_${dataset}_${fold}.out"
 err_file="./logs/${target}_${model}_${dataset}_${fold}.err"
-sbatch  --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 4-23:59 batch_jobs/predictions/single.sh $model $outer_splits $inner_splits $n_iter $target $dataset $fold
+sbatch  --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 1-00:00 batch_jobs/predictions/single.sh $model $outer_splits $inner_splits $n_iter $target $dataset $fold
