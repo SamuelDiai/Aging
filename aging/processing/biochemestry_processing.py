b117d5f298a1d2dcce711381b330363776de5f38
Samuel Diai <samuel@MacBook-Pro-de-Samuel.local>
2020-04-29 01:59:18 +0200

    fix instance selection

--- aging/processing/biochemestry_processing.py
+++ aging/processing/biochemestry_processing.py
@@ -11,7 +11,7 @@ Features used :
 def read_blood_biomarkers_data(**kwargs):
 	cols_features = list(range(30600, 30890 + 1, 10))
 	cols_filter = [ ]
-	instance = [0, 1, 2, 3]
+	instance = [0, 1]
 	cols_features.remove(30820)
 	cols_features.remove(30800)
 	return read_data(cols_features, cols_filter, instance, **kwargs)
@@ -19,13 +19,13 @@ def read_blood_biomarkers_data(**kwargs):
 def read_blood_count_data(**kwargs):
 	cols_features = list(range(30000, 30300 + 1, 10))
 	cols_filter = [ ]
-	instance = [0, 1, 2, 3]
+	instance = [0, 1, 2]
 	return read_data(cols_features, cols_filter, instance, **kwargs)
 
 def read_urine_biomarkers_data(**kwargs):
 	cols_features = list(range(30500, 30530 + 1, 10))
 	cols_filter = [ ]
-	instance = [0, 1, 2, 3]
+	instance = [0, 1]
 	return read_data(cols_features, cols_filter, instance, **kwargs)
 
 ## Aggregate
@@ -35,7 +35,7 @@ def read_blood_data(**kwargs):
 	cols_filter = [ ]
 	cols_features.remove(30820)
 	cols_features.remove(30800)
-	instance = [0, 1, 2, 3]
+	instance = [0, 1]
 	return read_data(cols_features, cols_filter, instance, **kwargs)
 
 # Super Aggregate
@@ -44,5 +44,5 @@ def read_urine_and_blood_data(**kwargs):
 	cols_filter = [ ]
 	cols_features.remove(30820)
 	cols_features.remove(30800)
-	instance = [0, 1, 2]
+	instance = [0, 1]
 	return read_data(cols_features, cols_filter, instance, **kwargs)
