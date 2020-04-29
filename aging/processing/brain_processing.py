201da850957a698b3ee1f5546ee2ca49e9a0e7de
Samuel Diai <samuel@MacBook-Pro-de-Samuel.local>
2020-04-29 03:06:30 +0200

    .

--- aging/processing/brain_processing.py
+++ aging/processing/brain_processing.py
@@ -22,14 +22,14 @@ def read_subcortical_volumes_data(**kwargs):
 	instance = [2, 3]
 	return read_data(cols_features, cols_filter, instance, **kwargs)
 
-def read_brain_data(**kwargs):
-	cols_features = list(range(25011, 25024 + 1)) + list(range(25782, 25920 + 1))
+def read_brain_dMRI_weighted_means_data(**kwargs):
+	cols_features = list(range(25488, 25730 + 1))
 	cols_filter = [ ]
 	instance = [2, 3]
 	return read_data(cols_features, cols_filter, instance, **kwargs)
-
-def read_brain_dMRI_weighted_means_data(**kwargs):
-	cols_features = list(range(25488, 25730 + 1))
+	
+def read_brain_data(**kwargs):
+	cols_features = list(range(25011, 25024 + 1)) + list(range(25782, 25920 + 1)) + list(range(25488, 25730 + 1))
 	cols_filter = [ ]
 	instance = [2, 3]
 	return read_data(cols_features, cols_filter, instance, **kwargs)
