b0bba1c0f3fdf8843ea17aa07a1337baa349ccd4
Samuel Diai <samuel@MacBook-Pro-de-Samuel.local>
2020-04-29 02:58:42 +0200

    .

--- aging/processing/mix_processing.py
+++ aging/processing/mix_processing.py
@@ -7,19 +7,19 @@ import pandas as pd
 
 def read_arterial_and_bp_data(**kwargs):
     df_1 = read_arterial_stiffness_data(**kwargs)
-    df_2 = read_blood_pressure_data(**kwargs).drop(columns = ['Age when attended assessment centre', 'Sex'])
+    df_2 = read_blood_pressure_data(**kwargs).drop(columns = ['Age when attended assessment centre', 'Sex', 'eid'])
     return df_1.join(df_2, how = 'inner')
 
 def read_cardiac_data(**kwargs):
     df_1 = read_arterial_stiffness_data(**kwargs)
-    df_2 = read_blood_pressure_data(**kwargs).drop(columns = ['Age when attended assessment centre', 'Sex'])
+    df_2 = read_blood_pressure_data(**kwargs).drop(columns = ['Age when attended assessment centre', 'Sex', 'eid'])
     res = df_1.join(df_2, how = 'inner')
-    df_3 = read_ecg_at_rest_data(**kwargs).drop(columns = ['Age when attended assessment centre', 'Sex'])
+    df_3 = read_ecg_at_rest_data(**kwargs).drop(columns = ['Age when attended assessment centre', 'Sex', 'eid'])
     return res.join(df_3, how = 'inner')
 
 def read_spiro_and_arterial_and_bp_data(**kwargs):
     df_1 = read_arterial_stiffness_data(**kwargs)
-    df_2 = read_blood_pressure_data(**kwargs).drop(columns = ['Age when attended assessment centre', 'Sex'])
+    df_2 = read_blood_pressure_data(**kwargs).drop(columns = ['Age when attended assessment centre', 'Sex', 'eid'])
     res = df_1.join(df_2, how = 'inner')
-    df_3 = read_spirometry_data(**kwargs).drop(columns = ['Age when attended assessment centre', 'Sex'])
+    df_3 = read_spirometry_data(**kwargs).drop(columns = ['Age when attended assessment centre', 'Sex', 'eid'])
     return res.join(df_3, how = 'inner')
