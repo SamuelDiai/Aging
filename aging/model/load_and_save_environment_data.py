0477b94f3fad12cabe7cfd0075769ea347e01290
Samuel Diai <samuel@MacBook-Pro-de-Samuel.local>
2020-05-07 18:51:24 +0200

    fix indent

--- aging/model/load_and_save_environment_data.py
+++ aging/model/load_and_save_environment_data.py
@@ -5,9 +5,9 @@ from ..processing import read_ethnicity_data
 from ..environment_processing.base_processing import path_features , path_predictions, path_inputs_env, path_target_residuals
 from ..environment_processing.disease_processing import read_infectious_diseases_data, read_infectious_disease_antigens_data
 from ..environment_processing.FamilyHistory import read_family_history_data
-from ..environment_processing.HealthAndMedicalHistory import read_breathing_data, read_cancer_screening_data, read_chest_pain_data, read_claudication_data, read_eye_history_data,
+from ..environment_processing.HealthAndMedicalHistory import read_breathing_data, read_cancer_screening_data, read_chest_pain_data, read_claudication_data, read_eye_history_data, \
                                                              read_general_health_data, read_general_pain_data, read_hearing_data, read_medication_data, read_mouth_teeth_data
-from ..environment_processing.LifestyleAndEnvironment import read_alcohol_data, read_diet_data, read_electronic_device_data, read_physical_activity_data, read_sexual_factors_data,
+from ..environment_processing.LifestyleAndEnvironment import read_alcohol_data, read_diet_data, read_electronic_device_data, read_physical_activity_data, read_sexual_factors_data,\
                                                              read_sleep_data, read_sun_exposure_data
 from ..environment_processing.PsychosocialFactors import read_mental_health_data, read_social_support_data
 from ..environment_processing.SocioDemographics import read_education_data, read_employment_data, read_household_data, read_other_sociodemographics_data
