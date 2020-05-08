68f4aa68368d34d55bbfdfc0fd8829b247b384dd
Samuel Diai <samuel@macbook-pro-de-samuel.home>
2020-05-08 04:32:23 +0200

    .

--- aging/model/load_and_save_environment_data.py
+++ aging/model/load_and_save_environment_data.py
@@ -233,17 +233,16 @@ def load_data(env_dataset, target_dataset, **kwargs):
     """
     ## Join on id by default
     df_env = load_data_env(env_dataset, **kwargs)
-    print(df_env.head())
+
     df_target = load_target_residuals(target_dataset, **kwargs)
-    print(df_target.head())
+
     df_ethnicities = load_ethnicity(**kwargs)
-    print(df_ethnicities.head())
+
 
     ## Try intersection
     df = df_env.join(df_target, how = 'inner', lsuffix='_dup', rsuffix='')
     columns_not_dup = df.columns[~df.columns.str.contains('_dup')]
     df = df[columns_not_dup]
-    print(df.head())
 
     ## If empty intersection join on eid
     if df.shape[0] == 0:
--- batch_jobs/ewas_predictions/python_caller_linear.py
+++ batch_jobs/ewas_predictions/python_caller_linear.py
@@ -21,45 +21,95 @@ hyperparameters['target_dataset'] = target_dataset
 hyperparameters['input_dataset'] = input_dataset
 print(hyperparameters)
 
-def normalise_dataset(df):
+ethnicity_cols = ['Do_not_know', 'Prefer_not_to_answer', 'NA', 'White', 'British',
+       'Irish', 'White_Other', 'Mixed', 'White_and_Black_Caribbean',
+       'White_and_Black_African', 'White_and_Asian', 'Mixed_Other', 'Asian',
+       'Indian', 'Pakistani', 'Bangladeshi', 'Asian_Other', 'Black',
+       'Caribbean', 'African', 'Black_Other', 'Chinese', 'Other_ethnicity',
+       'Other']
 
-    scaler_residual = StandardScaler()
-    scaler_residual.fit(df['residual'].values.reshape(-1, 1))
+# def normalise_dataset(df):
+#
+#     scaler_residual = StandardScaler()
+#     scaler_residual.fit(df['residual'].values.reshape(-1, 1))
+#
+# # Get categorical data apart from continous ones
+#     df_cat = df.select_dtypes(include=['int'])
+#     df_cont = df.drop(columns = df_cat.columns)
+#
+#     cols = df_cont.columns
+#     indexes = df_cont.index
+#
+#     # save scaler
+#     scaler = StandardScaler()
+#     scaler.fit(df_cont)
+#
+#     # Scale and create Dataframe
+#     array_rescaled =  scaler.transform(df_cont)
+#     df_rescaled = pd.DataFrame(array_rescaled, columns = cols, index = indexes).join(df_cat)
+#
+#     return df_rescaled, scaler_residual
 
-# Get categorical data apart from continous ones
-    df_cat = df.select_dtypes(include=['int'])
-    df_cont = df.drop(columns = df_cat.columns)
-
-    cols = df_cont.columns
-    indexes = df_cont.index
-
-    # save scaler
-    scaler = StandardScaler()
-    scaler.fit(df_cont)
-
-    # Scale and create Dataframe
-    array_rescaled =  scaler.transform(df_cont)
-    df_rescaled = pd.DataFrame(array_rescaled, columns = cols, index = indexes).join(df_cat)
-
-    return df_rescaled, scaler_residual
+df = load_data(input_dataset, target_dataset, nrows = 20000).drop(columns = ['eid'])
+#df_rescaled, scaler_residual = normalise_dataset(df)
+columns_age_sex_ethnicity = ['Age', 'Sex'] + ethnicity_cols
+cols_except_age_sex_residual_ethnicty = df.drop(columns = ['residual', 'Age', 'Sex'] + ethnicity_cols).columns
 
 
-df = load_data(input_dataset, target_dataset)
-df_rescaled, scaler_residual = normalise_dataset(df)
-cols_except_age_sex_residual = df_rescaled.drop(columns = ['residual', 'Age', 'Sex']).columns
+d = pd.DataFrame(columns = ['env_feature_name', 'target_dataset_name', 'p_val', 'corr_value', 'size_na_dropped'])
+for column in cols_except_age_sex_residual_ethnicty:
+    df_col = df[[column, 'residual'] + columns_age_sex_ethnicity]
+    df_col = df_col.dropna()
 
+    lin_residual = LinearRegression()
+    lin_residual.fit(df_col[columns_age_sex_ethnicity].values, df_col['residual'].values)
+    res_residual = df_col['residual'].values - lin_residual.predict(df_col[columns_age_sex_ethnicity].values)
 
-d = pd.DataFrame(columns = ['env_feature_name', 'target_dataset_name', 'p_val', 'corr_value'])
-lin_residual = LinearRegression()
-lin_residual.fit(df_rescaled[['Age', 'Sex']].values, df_rescaled['residual'].values)
-res_residual = df_rescaled['residual'].values - lin_residual.predict(df[['Age', 'Sex']].values)
-
-for column in cols_except_age_sex_residual:
     lin_feature = LinearRegression()
-    lin_feature.fit(df_rescaled[['Age', 'Sex']].values, df_rescaled[column].values)
-    res_feature = df_rescaled[column].values - lin_feature.predict(df_rescaled[['Age', 'Sex']].values)
+    lin_feature.fit(df_col[columns_age_sex_ethnicity].values, df_col[column].values)
+    res_feature = df_col[column].values - lin_feature.predict(df_col[columns_age_sex_ethnicity].values)
 
     corr, p_val = pearsonr(res_residual, res_feature)
-    d = d.append({'env_feature_name' : column, 'target_dataset_name' : target_dataset, 'p_val' : p_val, 'corr_value' : corr}, ignore_index = True)
+    d = d.append({'env_feature_name' : column, 'target_dataset_name' : target_dataset, 'p_val' : p_val, 'corr_value' : corr, 'size_na_dropped' : df_col.shape[0]}, ignore_index = True)
+d.to_csv('/n/groups/patel/samuel/EWAS/linear_output_v2/linear_correlations_%s_%s.csv' % (input_dataset, target_dataset), index=False)
 
-d.to_csv('/n/groups/patel/samuel/EWAS/linear_output/linear_correlations_%s_%s.csv' % (input_dataset, target_dataset), index=False)
+
+
+#
+#
+#
+#
+#
+# lin_residual = LinearRegression()
+# lin_residual.fit(df[['Age', 'Sex'] + ].values, df_rescaled['residual'].values)
+# res_residual = df_rescaled['residual'].values - lin_residual.predict(df[['Age', 'Sex']].values)
+#
+#
+#
+#
+# lin_residual = LinearRegression()
+# columns_age_sex_ethnicity = ['Age', 'Sex'] + list(df_ethicity.columns)
+# lin_residual.fit(df_all[columns_age_sex_ethnicity].values, df_all['residual'].values)
+# res_residual = df_all['residual'].values - lin_residual.predict(df_all[columns_age_sex_ethnicity].values)
+#
+# for column in df_diagnose.columns:
+#     lin_feature = LinearRegression()
+#     lin_feature.fit(df_all[columns_age_sex_ethnicity].values, df_all[column].values)
+#     res_feature = df_all[column].values - lin_feature.predict(df_all[columns_age_sex_ethnicity].values)
+#
+#     corr, p_val = pearsonr(res_residual, res_feature)
+#     d = d.append({'env_feature_name' : column, 'target_dataset_name' : target_dataset, 'p_val' : p_val, 'corr_value' : corr}, ignore_index = True)
+#
+#
+#
+#
+#
+# for column in cols_except_age_sex_residual:
+#     lin_feature = LinearRegression()
+#     lin_feature.fit(df_rescaled[['Age', 'Sex']].values, df_rescaled[column].values)
+#     res_feature = df_rescaled[column].values - lin_feature.predict(df_rescaled[['Age', 'Sex']].values)
+#
+#     corr, p_val = pearsonr(res_residual, res_feature)
+#     d = d.append({'env_feature_name' : column, 'target_dataset_name' : target_dataset, 'p_val' : p_val, 'corr_value' : corr}, ignore_index = True)
+#
+#
