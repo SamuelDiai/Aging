15ebd19645052a12041b58e035f26a37e84b3b59
Samuel Diai <samuel@MacBook-Pro-de-Samuel.local>
2020-05-12 02:18:41 +0200

    .

--- batch_jobs/ewas_predictions/parallel.sh
+++ batch_jobs/ewas_predictions/parallel.sh
@@ -5,12 +5,12 @@ target_datasets=( "Blood" )
 #target_datasets=( "BrainGreyMatterVolumes" "BrainSubcorticalVolumes" "Brain" "Heart" "HeartSize" "HeartPWA" "AnthropometryImpedance" "UrineBiochemestry" "BloodBiochemestry" "BloodCount" "Blood" "UrineAndBlood" "EyeAutorefraction" "EyeAcuity" "EyeIntraoculaPressure" "Eye" "Spirometry" "BloodPressure" "AnthropometryBodySize" "Anthropometry" "ArterialStiffness" "ArterialAndBloodPressure" "SpiroAndArterialAndBp" )
 #input_datasets=( "Education" "medical_diagnoses_I" )
 #input_datasets=( "Alcohol" "Diet" "Education" "ElectronicDevices" "Employment" "FamilyHistory" "Eyesight" "Mouth" "GeneralHealth" "Breathing" "Claudification" "GeneralPain" "ChestPain" "CancerScreening" "Medication" "Hearing" "Household" "MentalHealth" "OtherSociodemographics" "PhysicalActivity" "SexualFactors" "Sleep" "SocialSupport" "SunExposure" "medical_diagnoses_A" "medical_diagnoses_B" "medical_diagnoses_C" "medical_diagnoses_D" "medical_diagnoses_E" "medical_diagnoses_F" "medical_diagnoses_G" "medical_diagnoses_H" "medical_diagnoses_I" "medical_diagnoses_J" "medical_diagnoses_K" "medical_diagnoses_L" "medical_diagnoses_M" "medical_diagnoses_N" "medical_diagnoses_O" "medical_diagnoses_P" "medical_diagnoses_Q" "medical_diagnoses_R" "medical_diagnoses_S" "medical_diagnoses_T" "medical_diagnoses_U" "medical_diagnoses_V" "medical_diagnoses_W" "medical_diagnoses_X" "medical_diagnoses_Y" "medical_diagnoses_Z" )
-input_datasets=( "Alcohol" "medical_diagnoses_K" )
+input_datasets=( "Alcohol" )
 
 outer_splits=3
 inner_splits=2
-n_iter=1
-n_splits=5
+n_iter=2
+n_splits=2
 
 memory=8G
 n_cores=1
