26726cce944d43b2341e6f5f61a9c22f16b1b2e7
Samuel Diai <samuel@MacBook-Pro-de-Samuel.local>
2020-05-15 02:20:47 +0200

    Add dissimilarity + linAge

--- batch_jobs/ewas_predictions/parallel.sh
+++ batch_jobs/ewas_predictions/parallel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #models=( "ElasticNet" )
 models=( "Xgboost" "RandomForest" "GradientBoosting" "LightGbm" "NeuralNetwork" "ElasticNet" )
-target_datasets=( "HeartImages" "LiverImages" )
+target_datasets=( "Anthropometry" "AnthropometryBodySize" "AnthropometryImpedance" "ArterialAndBloodPressure" "ArterialStiffness" "Blood" "BloodBiochemestry" "BloodCount" "BloodPressure" "Brain" "BrainGreyMatterVolumes" "BrainSubcorticalVolumes" "ECGAtRest" "Eye" "EyeAcuity" "EyeAutorefraction" "EyeIntraoculaPressure" "Heart" "HeartPWA" "HeartSize" "SpiroAndArterialAndBp" "Spirometry" "UrineAndBlood" "UrineBiochemestry" )
 #target_datasets=( "BrainGreyMatterVolumes" "BrainSubcorticalVolumes" "Brain" "Heart" "HeartSize" "HeartPWA" "AnthropometryImpedance" "UrineBiochemestry" "BloodBiochemestry" "BloodCount" "Blood" "UrineAndBlood" "EyeAutorefraction" "EyeAcuity" "EyeIntraoculaPressure" "Eye" "Spirometry" "BloodPressure" "AnthropometryBodySize" "Anthropometry" "ArterialStiffness" "ArterialAndBloodPressure" "SpiroAndArterialAndBp" )
-#input_datasets=( "Education" "medical_diagnoses_I" )
-input_datasets=( "Brain" "Eye" "Heart" "Blood" "UrineAndBlood" "ECGAtRest" "Anthropometry" "SpiroAndArterialAndBp" "CarotidUltrasound" )
+#input_datasets=( "Education" )
+input_datasets=( "Alcohol" "Anthropometry" "AnthropometryBodySize" "AnthropometryImpedance" "ArterialAndBloodPressure" "ArterialStiffness" "Blood" "BloodBiochemestry" "BloodCount" "BloodPressure" "Brain" "BrainGreyMatterVolumes" "BrainSubcorticalVolumes" "BraindMRIWeightedMeans" "Breathing" "CancerScreening" "CarotidUltrasound" "ChestPain" "Claudification" "Diet" "ECGAtRest" "Education" "ElectronicDevices" "Employment" "Eye" "EyeAcuity" "EyeAutorefraction" "EyeIntraoculaPressure" "Eyesight" "FamilyHistory" "GeneralHealth" "GeneralPain" "Hearing" "Heart" "HeartPWA" "HeartSize" "Household" "Medication" "MentalHealth" "Mouth" "OtherSociodemographics" "PhysicalActivity" "SexualFactors" "Sleep" "SocialSupport" "SpiroAndArterialAndBp" "Spirometry" "SunExposure" "UrineAndBlood" "UrineBiochemestry" "medical_diagnoses_A" "medical_diagnoses_B" "medical_diagnoses_C" "medical_diagnoses_D" "medical_diagnoses_E" "medical_diagnoses_F" "medical_diagnoses_G" "medical_diagnoses_H" "medical_diagnoses_I" "medical_diagnoses_J" "medical_diagnoses_K" "medical_diagnoses_L" "medical_diagnoses_M" "medical_diagnoses_N" "medical_diagnoses_O" "medical_diagnoses_P" "medical_diagnoses_Q" "medical_diagnoses_R" "medical_diagnoses_S" "medical_diagnoses_T" "medical_diagnoses_U" "medical_diagnoses_V" "medical_diagnoses_W" "medical_diagnoses_X" "medical_diagnoses_Y" "medical_diagnoses_Z" )
 #input_datasets=( "Medication" )
 
 outer_splits=5
@@ -26,32 +26,32 @@ do
 		err_file="./logs/${target_dataset}_${input_dataset}.err"
 
 		sbatch --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p short -t 0-11:59 batch_jobs/ewas_predictions/linear_study.sh $target_dataset $input_dataset
-		for model in "${models[@]}"
-		do
-			declare -a IDs=()
-			for ((fold=0; fold <= $outer_splits-1; fold++))
-			do
-			  job_name="${target_dataset}_${model}_${input_dataset}_${fold}.job"
-			  out_file="./logs/${target_dataset}_${model}_${input_dataset}_${fold}.out"
-			  err_file="./logs/${target_dataset}_${model}_${input_dataset}_${fold}.err"
-				ID=$(sbatch --parsable --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 4-23:59 batch_jobs/ewas_predictions/single.sh $model $outer_splits $inner_splits $n_iter $target_dataset $input_dataset $fold)
-				IDs+=($ID)
-			done
-			if [ $model != "NeuralNetwork" ]
-			then
-			   job_name="${target_dataset}_${model}_${input_dataset}_features.job"
-				 out_file="./logs/${target_dataset}_${model}_${input_dataset}_features.out"
-				 err_file="./logs/${target_dataset}_${model}_${input_dataset}_features.err"
-         sbatch --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 4-23:59 batch_jobs/ewas_predictions/single_features.sh $model $n_iter $target_dataset $input_dataset $n_splits
-			else
-					:
-			fi
-			job_name="${target_dataset}_${model}_${input_dataset}_postprocessing.job"
-			out_file="./logs/${target_dataset}_${model}_${input_dataset}_postprocessing.out"
-			err_file="./logs/${target_dataset}_${model}_${input_dataset}_postprocessing.err"
-
-			printf -v joinedIDS '%s:' "${IDs[@]}"
-			sbatch --dependency=afterok:${joinedIDS%:} --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p short -t 0-11:59 batch_jobs/ewas_predictions/postprocessing.sh $model $target_dataset $input_dataset $outer_splits
+		#for model in "${models[@]}"
+		#do
+		#	declare -a IDs=()
+		#	for ((fold=0; fold <= $outer_splits-1; fold++))
+		#	do
+		#	  job_name="${target_dataset}_${model}_${input_dataset}_${fold}.job"
+		#	  out_file="./logs/${target_dataset}_${model}_${input_dataset}_${fold}.out"
+		#	  err_file="./logs/${target_dataset}_${model}_${input_dataset}_${fold}.err"
+		#		ID=$(sbatch --parsable --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 4-23:59 batch_jobs/ewas_predictions/single.sh $model $outer_splits $inner_splits $n_iter $target_dataset $input_dataset $fold)
+		#		IDs+=($ID)
+		#	done
+		#	if [ $model != "NeuralNetwork" ]
+		#	then
+		#	   job_name="${target_dataset}_${model}_${input_dataset}_features.job"
+		#		 out_file="./logs/${target_dataset}_${model}_${input_dataset}_features.out"
+		#		 err_file="./logs/${target_dataset}_${model}_${input_dataset}_features.err"
+    #     sbatch --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 4-23:59 batch_jobs/ewas_predictions/single_features.sh $model $n_iter $target_dataset $input_dataset $n_splits
+		#	else
+		#			:
+		#	fi
+		#	job_name="${target_dataset}_${model}_${input_dataset}_postprocessing.job"
+		#	out_file="./logs/${target_dataset}_${model}_${input_dataset}_postprocessing.out"
+		#	err_file="./logs/${target_dataset}_${model}_${input_dataset}_postprocessing.err"
+#
+		#	printf -v joinedIDS '%s:' "${IDs[@]}"
+		#	sbatch --dependency=afterok:${joinedIDS%:} --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p short -t 0-11:59 batch_jobs/ewas_predictions/postprocessing.sh $model $target_dataset $input_dataset $outer_splits
 		done
 	done
 done
--- batch_jobs/ewas_predictions/python_caller_linear.py
+++ batch_jobs/ewas_predictions/python_caller_linear.py
@@ -3,14 +3,14 @@ import sys
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
-from scipy.stats import pearsonr
+from scipy.stats import pearsonr, linregress
 
 if sys.platform == 'linux':
     sys.path.append('/n/groups/patel/samuel/Aging')
 elif sys.platform == 'darwin':
     sys.path.append('/Users/samuel/Desktop/Aging')
 
-from aging.model.load_and_save_environment_data import load_data, ETHNICITY_COLS
+from aging.model.load_and_save_environment_data import load_data, ETHNICITY_COLS, load_data_env
 from aging.environment_processing.base_processing import path_output_linear_study
 
 target_dataset = sys.argv[1]
@@ -23,6 +23,10 @@ print(hyperparameters)
 
 
 df = load_data(input_dataset, target_dataset).drop(columns = ['eid'])
+
+
+
+## Linear EWAS :
 #df_rescaled, scaler_residual = normalise_dataset(df)
 columns_age_sex_ethnicity = ['Age', 'Sex'] + ETHNICITY_COLS
 cols_except_age_sex_residual_ethnicty = df.drop(columns = ['residual', 'Age', 'Sex'] + ETHNICITY_COLS).columns
@@ -44,3 +48,45 @@ for column in cols_except_age_sex_residual_ethnicty:
     corr, p_val = pearsonr(res_residual, res_feature)
     d = d.append({'env_feature_name' : column, 'target_dataset_name' : target_dataset, 'p_val' : p_val, 'corr_value' : corr, 'size_na_dropped' : df_col.shape[0]}, ignore_index = True)
 d.to_csv(path_output_linear_study + 'linear_correlations_%s_%s.csv' % (input_dataset, target_dataset), index=False)
+
+
+## See effect of Age per feature
+d2 = pd.DataFrame(columns = ['target_dataset_name', 'env_feature_name', 'p_val', 'r_val', 'cslope'])
+
+columns_sex_ethnicity = ['Sex'] + ETHNICITY_COLS
+for column in cols_except_age_sex_residual_ethnicty:
+    df_col2 = df[[column] + columns_age_sex_ethnicity]
+    df_col2 = df_col2.dropna()
+
+    lin_residual2 = LinearRegression()
+    lin_residual2.fit(df_col2[columns_sex_ethnicity].values, df_col2['Age'].values)
+    res_residual2 = df_col2['Age'].values - lin_residual2.predict(df_col2[columns_sex_ethnicity].values)
+
+    lin_feature2 = LinearRegression()
+    lin_feature2.fit(df_col2[columns_sex_ethnicity].values, df_col2[column].values)
+    res_feature2 = df_col2[column].values - lin_feature2.predict(df_col2[columns_sex_ethnicity].values)
+
+    cslope, intercept, r_value, p_value, std_err = linregress(res_feature2, res_residual2)
+    d2 = d2.append({'target_dataset_name' : target_dataset,'env_name' : input_dataset, 'env_feature_name' : column,   'p_val' : p_value, 'r_val' : r_value, 'cslope' : cslope, 'intercept' : intercept}, ignore_index = True)
+d2.to_csv('/n/groups/patel/samuel/EWAS/LinearAge/' +  'linear_age_%s_%s.csv' % (input_dataset, target_dataset), index = False)
+
+
+
+## Compute dissimilarity :
+
+df_env = load_data_env(input_dataset).drop(columns = ['eid'])
+df_target = load_data_env(target_dataset).drop(columns = ['eid'])
+large_join = df_env.join(df_target)
+large_join_shape = large_join.shape[0]
+tiny_join = df_env.join(df_target).dropna()
+tiny_join_shape = tiny_join.shape[0]
+
+
+if large_join_shape == 0:
+    quotient = 0
+else :
+    quotient = large_join_shape / tiny_join_shape
+
+
+df_res = pd.DataFrame({'Target Dataset': [target_dataset],  'Environmental Dataset' : [input_dataset], 'Intersection' : [tiny_join_shape], 'Union' : [large_join_shape], 'Dissimilartiy': [quotient]})
+df_res.to_csv('/n/groups/patel/samuel/EWAS/SampleSizes.csv', mode = 'a', index = False)
