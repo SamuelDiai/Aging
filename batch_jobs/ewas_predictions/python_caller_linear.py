571aece2b4f41ce3fe1bf21c924848c828b16fa1
Samuel Diai <samuel@MacBook-Pro-de-Samuel.local>
2020-05-15 12:20:32 +0200

    just dissim

--- batch_jobs/ewas_predictions/python_caller_linear.py
+++ batch_jobs/ewas_predictions/python_caller_linear.py
@@ -22,60 +22,57 @@ hyperparameters['input_dataset'] = input_dataset
 print(hyperparameters)
 
 
-df = load_data(input_dataset, target_dataset).drop(columns = ['eid'])
-
-
-
-## Linear EWAS :
-#df_rescaled, scaler_residual = normalise_dataset(df)
-columns_age_sex_ethnicity = ['Age', 'Sex'] + ETHNICITY_COLS
-cols_except_age_sex_residual_ethnicty = df.drop(columns = ['residual', 'Age', 'Sex'] + ETHNICITY_COLS).columns
-
-
-d = pd.DataFrame(columns = ['env_feature_name', 'target_dataset_name', 'p_val', 'corr_value', 'size_na_dropped'])
-for column in cols_except_age_sex_residual_ethnicty:
-    df_col = df[[column, 'residual'] + columns_age_sex_ethnicity]
-    df_col = df_col.dropna()
-
-    lin_residual = LinearRegression()
-    lin_residual.fit(df_col[columns_age_sex_ethnicity].values, df_col['residual'].values)
-    res_residual = df_col['residual'].values - lin_residual.predict(df_col[columns_age_sex_ethnicity].values)
-
-    lin_feature = LinearRegression()
-    lin_feature.fit(df_col[columns_age_sex_ethnicity].values, df_col[column].values)
-    res_feature = df_col[column].values - lin_feature.predict(df_col[columns_age_sex_ethnicity].values)
-
-    corr, p_val = pearsonr(res_residual, res_feature)
-    d = d.append({'env_feature_name' : column, 'target_dataset_name' : target_dataset, 'p_val' : p_val, 'corr_value' : corr, 'size_na_dropped' : df_col.shape[0]}, ignore_index = True)
-d.to_csv(path_output_linear_study + 'linear_correlations_%s_%s.csv' % (input_dataset, target_dataset), index=False)
-
-
-## See effect of Age per feature
-d2 = pd.DataFrame(columns = ['target_dataset_name', 'env_feature_name', 'p_val', 'r_val', 'cslope'])
-
-columns_sex_ethnicity = ['Sex'] + ETHNICITY_COLS
-for column in cols_except_age_sex_residual_ethnicty:
-    df_col2 = df[[column] + columns_age_sex_ethnicity]
-    df_col2 = df_col2.dropna()
-
-    lin_residual2 = LinearRegression()
-    lin_residual2.fit(df_col2[columns_sex_ethnicity].values, df_col2['Age'].values)
-    res_residual2 = df_col2['Age'].values - lin_residual2.predict(df_col2[columns_sex_ethnicity].values)
-
-    lin_feature2 = LinearRegression()
-    lin_feature2.fit(df_col2[columns_sex_ethnicity].values, df_col2[column].values)
-    res_feature2 = df_col2[column].values - lin_feature2.predict(df_col2[columns_sex_ethnicity].values)
-
-    cslope, intercept, r_value, p_value, std_err = linregress(res_feature2, res_residual2)
-    d2 = d2.append({'target_dataset_name' : target_dataset,'env_name' : input_dataset, 'env_feature_name' : column,   'p_val' : p_value, 'r_val' : r_value, 'cslope' : cslope, 'intercept' : intercept}, ignore_index = True)
-d2.to_csv('/n/groups/patel/samuel/EWAS/LinearAge/' +  'linear_age_%s_%s.csv' % (input_dataset, target_dataset), index = False)
+# df = load_data(input_dataset, target_dataset).drop(columns = ['eid'])
+#
+#
+#
+# ## Linear EWAS :
+# #df_rescaled, scaler_residual = normalise_dataset(df)
+# columns_age_sex_ethnicity = ['Age', 'Sex'] + ETHNICITY_COLS
+# cols_except_age_sex_residual_ethnicty = df.drop(columns = ['residual', 'Age', 'Sex'] + ETHNICITY_COLS).columns
+#
+#
+# d = pd.DataFrame(columns = ['env_feature_name', 'target_dataset_name', 'p_val', 'corr_value', 'size_na_dropped'])
+# for column in cols_except_age_sex_residual_ethnicty:
+#     df_col = df[[column, 'residual'] + columns_age_sex_ethnicity]
+#     df_col = df_col.dropna()
+#
+#     lin_residual = LinearRegression()
+#     lin_residual.fit(df_col[columns_age_sex_ethnicity].values, df_col['residual'].values)
+#     res_residual = df_col['residual'].values - lin_residual.predict(df_col[columns_age_sex_ethnicity].values)
+#
+#     lin_feature = LinearRegression()
+#     lin_feature.fit(df_col[columns_age_sex_ethnicity].values, df_col[column].values)
+#     res_feature = df_col[column].values - lin_feature.predict(df_col[columns_age_sex_ethnicity].values)
+#
+#     corr, p_val = pearsonr(res_residual, res_feature)
+#     d = d.append({'env_feature_name' : column, 'target_dataset_name' : target_dataset, 'p_val' : p_val, 'corr_value' : corr, 'size_na_dropped' : df_col.shape[0]}, ignore_index = True)
+# d.to_csv(path_output_linear_study + 'linear_correlations_%s_%s.csv' % (input_dataset, target_dataset), index=False)
+#
+#
+# ## See effect of Age per feature
+# d2 = pd.DataFrame(columns = ['target_dataset_name', 'env_feature_name', 'p_val', 'r_val', 'cslope'])
+#
+# columns_sex_ethnicity = ['Sex'] + ETHNICITY_COLS
+# for column in cols_except_age_sex_residual_ethnicty:
+#     df_col2 = df[[column] + columns_age_sex_ethnicity]
+#     df_col2 = df_col2.dropna()
+#
+#     lin_residual2 = LinearRegression()
+#     lin_residual2.fit(df_col2[columns_sex_ethnicity].values, df_col2['Age'].values)
+#     res_residual2 = df_col2['Age'].values - lin_residual2.predict(df_col2[columns_sex_ethnicity].values)
+#
+#     lin_feature2 = LinearRegression()
+#     lin_feature2.fit(df_col2[columns_sex_ethnicity].values, df_col2[column].values)
+#     res_feature2 = df_col2[column].values - lin_feature2.predict(df_col2[columns_sex_ethnicity].values)
+#
+#     cslope, intercept, r_value, p_value, std_err = linregress(res_feature2, res_residual2)
+#     d2 = d2.append({'target_dataset_name' : target_dataset,'env_name' : input_dataset, 'env_feature_name' : column,   'p_val' : p_value, 'r_val' : r_value, 'cslope' : cslope, 'intercept' : intercept}, ignore_index = True)
+# d2.to_csv('/n/groups/patel/samuel/EWAS/LinearAge/' +  'linear_age_%s_%s.csv' % (input_dataset, target_dataset), index = False)
 
 
 
 ## Compute dissimilarity :
-del df
-del d
-del d2
 df_env = load_data_env(input_dataset).drop(columns = ['eid'])
 df_target = load_data_env(target_dataset).drop(columns = ['eid'])
 large_join = df_env.join(df_target, how = 'outer', lsuffix = '_l', rsuffix = '_r')
