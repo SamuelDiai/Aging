f027ac98c3603ca56206cfaecc2a488544c61d86
Samuel Diai <samuel@MacBook-Pro-de-Samuel.local>
2020-06-05 00:01:23 +0200

    git pull

--- batch_jobs/ewas_predictions/parallel.sh
+++ batch_jobs/ewas_predictions/parallel.sh
@@ -38,7 +38,7 @@ counter_input=0
 job_name="Create_raw_data.job"
 out_file="./logs/Create_raw_data.out"
 err_file="./logs/Create_raw_data.err"
-ID_raw=$(sbatch --parsable  --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=50G -c $n_cores -p short -t 0-11:59 batch_jobs/ewas_predictions/create_raw_data.sh)
+ID_raw=$(sbatch --parsable  --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=32G -c $n_cores -p short -t 0-11:59 batch_jobs/ewas_predictions/create_raw_data.sh)
 
 
 # printf -v joinedIDsLoads '%s:' "${IDsLoads[@]}"
@@ -52,7 +52,7 @@ n_cores_inputing=16
 job_name="Input_data.job"
 out_file="./logs/Input_data.out"
 err_file="./logs/Input_data.err"
-ID_inputed=$(sbatch --parsable --dependency=afterok:$ID_raw --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=50G -c 16 -p short -t 0-11:59 batch_jobs/ewas_predictions/input_data.sh $n_cores_inputing)
+ID_inputed=$(sbatch --parsable --dependency=afterok:$ID_raw --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=32G -c 16 -p short -t 0-11:59 batch_jobs/ewas_predictions/input_data.sh $n_cores_inputing)
 
 #
 # for input_dataset in "${input_datasets[@]}"
