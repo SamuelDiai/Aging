fc3c2e7ce1e21fc23c37db5fb62c13e6b803845a
Samuel Diai <samuel@MacBook-Pro-de-Samuel.local>
2020-06-12 13:23:39 +0200

    .

--- batch_jobs/test_NN/parallel.sh
+++ batch_jobs/test_NN/parallel.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
-#datasets=( 'BloodBiochemestry' 'EyeAcuity' 'BraindMRIWeightedMeans' 'HearingTest' 'HearingTest' )
-datasets=( 'HeartPWA' )
-#architectures=( '1024;512;256;128' '512;256;128' '500;250' '300;100' '100;50' '30;10' )
-architectures=( '30;10' )
-for dataset in "${datasets[@]}"
+# #datasets=( 'BloodBiochemestry' 'EyeAcuity' 'BraindMRIWeightedMeans' 'HearingTest' 'HearingTest' )
+# dataset=( 'BraindMRIWeightedMeans' )
+# #architectures=( '1024;512;256;128' '512;256;128' '500;250' '300;100' '100;50' '30;10' )
+# architectures=( '30;10' )
+# for dataset in "${datasets[@]}"
+# do
+#   for architecture in "${architectures[@]}"
+#   do
+#     job_name="test_${dataset}_${architecture}.job"
+#     out_file="./logs/test_${dataset}_${architecture}.out"
+#     err_file="./logs/test_${dataset}_${architecture}.err"
+#     sbatch --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=16G -c 1 -p short -t 0-11:59 batch_jobs/test_NN/single.sh $dataset $architecture
+#   done
+# done
+
+
+alphas=( '1e-3' '1e-2' '1e-1' '1e0' '1e1' '1e2' '1e3' )
+dataset='BraindMRIWeightedMeans'
+architecture='1024;512;256;128'
+for alpha in "${alphas[@]}"
 do
-  for architecture in "${architectures[@]}"
-  do
-    job_name="test_${dataset}_${architecture}.job"
-    out_file="./logs/test_${dataset}_${architecture}.out"
-    err_file="./logs/test_${dataset}_${architecture}.err"
-    sbatch --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=16G -c 1 -p short -t 0-11:59 batch_jobs/test_NN/single.sh $dataset $architecture
-  done
+    job_name="test_${dataset}_${alpha}.job"
+    out_file="./logs/test_${dataset}_${alpha}.out"
+    err_file="./logs/test_${dataset}_${alpha}.err"
+    sbatch --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=16G -c 1 -p short -t 0-11:59 batch_jobs/test_NN/single.sh $dataset $architecture $alpha
 done
--- batch_jobs/test_NN/python_caller.py
+++ batch_jobs/test_NN/python_caller.py
@@ -11,6 +11,7 @@ from sklearn.metrics import r2_score, f1_score
 import numpy as np
 dataset = sys.argv[1]
 architecture = sys.argv[2]
+alpha = float(sys.argv[3])
 
 df_path = '/n/groups/patel/samuel/save_final_inputs/' + dataset + '.csv'
 
@@ -50,7 +51,7 @@ X = df_rescaled.drop(columns = ['eid', 'Age when attended assessment centre'])
 y = df_rescaled['Age when attended assessment centre']
 params = {
                     'batch_size': 5000,
-                    'activation': 'relu', 'max_iter' : 200, 'tol' : 1e-7}
+                    'activation': 'relu', 'max_iter' : 200, 'tol' : 1e-7, 'alpha' : alpha}
 
 hidd_layer = [int(elem) for elem in architecture.split(';')]
 
@@ -58,46 +59,51 @@ params['hidden_layer_sizes'] = hidd_layer
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=3)
-X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=3) # 0.25 x 0.8 = 0.2
+#X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=3) # 0.25 x 0.8 = 0.2
 
 space = {'alpha': hp.loguniform('alpha', low = np.log(1e-5), high = np.log(1e-1))}
+estimator_ = MLPRegressor(**params)
+estimator_.fit(X_train.values, y_train.values)
 
-def objective(hyperparameters):
-    estimator_ = MLPRegressor(**params)
-                ## Set hyperparameters to the model :
-    for key, value in hyperparameters.items():
-        if hasattr(estimator_, key):
-            setattr(estimator_, key, value)
-        else :
-            continue
-    estimator_.fit(X_train.values, y_train.values)
-    y_val_estimed = estimator_.predict(X_val.values)
-    scores = r2_score(y_val, y_val_estimed)
+training_score = estimator_.score(X_train.values, y_train.values)
+y_test_estimed = estimator_.predict(X_test.values)
+scores = r2_score(y_test, y_test_estimed)
+# def objective(hyperparameters):
+#     estimator_ = MLPRegressor(**params)
+#                 ## Set hyperparameters to the model :
+#     for key, value in hyperparameters.items():
+#         if hasattr(estimator_, key):
+#             setattr(estimator_, key, value)
+#         else :
+#             continue
+#     estimator_.fit(X_train.values, y_train.values)
+#     y_val_estimed = estimator_.predict(X_val.values)
+#     scores = r2_score(y_val, y_val_estimed)
+#
+#     y_train_estimed = estimator_.predict(X_train.values)
+#     score_train = r2_score(y_train, y_train_estimed)
+#
+#
+#     return {'status' : STATUS_OK, 'loss' : -scores, 'attachments' :  {'score_train_val_model_param' :(scores, score_train, estimator_, hyperparameters['alpha'])}}
+#
+# trials = Trials()
+# best = fmin(objective, space, algo = tpe.suggest, max_evals=5, trials = trials)
+# best_params = space_eval(space, best)
 
-    y_train_estimed = estimator_.predict(X_train.values)
-    score_train = r2_score(y_train, y_train_estimed)
+# d = pd.DataFrame(columns = ['score_val', 'score_train', 'estim', 'alpha'])
+# for key, value in trials.attachments.items():
+#     score_val, score_train, estim, alpha = value
+#     d = d.append({'score_val' : score_val, 'score_train' : score_train, 'estim' : estim, 'alpha' : alpha}, ignore_index = True)
+#
+#
+# score_val_max, score_train_max, estim_max, alpha_max = d.iloc[d['score_val'].idxmax(axis = 1)]
+# predict_test = estim_max.predict(X_test.values)
+#
+# score_test = r2_score(y_test, predict_test)
 
-
-    return {'status' : STATUS_OK, 'loss' : -scores, 'attachments' :  {'score_train_val_model_param' :(scores, score_train, estimator_, hyperparameters['alpha'])}}
-
-trials = Trials()
-best = fmin(objective, space, algo = tpe.suggest, max_evals=5, trials = trials)
-best_params = space_eval(space, best)
-
-d = pd.DataFrame(columns = ['score_val', 'score_train', 'estim', 'alpha'])
-for key, value in trials.attachments.items():
-    score_val, score_train, estim, alpha = value
-    d = d.append({'score_val' : score_val, 'score_train' : score_train, 'estim' : estim, 'alpha' : alpha}, ignore_index = True)
-
-
-score_val_max, score_train_max, estim_max, alpha_max = d.iloc[d['score_val'].idxmax(axis = 1)]
-predict_test = estim_max.predict(X_test.values)
-
-score_test = r2_score(y_test, predict_test)
-
-test = test.append({'dataset' : dataset, 'sample_size' : sample_size, 'num features' : num_features, 'training score' : score_train_max, 'validation score' : score_val_max, 'test score' : score_test, 'alpha max' : alpha_max}, ignore_index = True)
+test = test.append({'dataset' : dataset, 'sample_size' : sample_size, 'num features' : num_features, 'training score' : training_score, 'test score' : scores,, 'alpha' : alpha}, ignore_index = True)
 test.to_csv('/n/groups/patel/samuel/res_NN/res_2_%s_%s' % (dataset, architecture))
 
 
-df_res = pd.DataFrame({'pred' : predict_test, 'real' : y_test})
-df_res.to_csv('/n/groups/patel/samuel/res_NN/pred_2_%s_%s' % (dataset, architecture))
+# df_res = pd.DataFrame({'pred' : predict_test, 'real' : y_test})
+# df_res.to_csv('/n/groups/patel/samuel/res_NN/pred_2_%s_%s' % (dataset, architecture))
