f6e2c0b482f98175e6f7ef6aa6face924c3760ee
Samuel Diai <samuel@MacBook-Pro-de-Samuel.local>
2020-06-12 04:14:58 +0200

    .

--- batch_jobs/test_NN/parallel.sh
+++ batch_jobs/test_NN/parallel.sh
@@ -7,9 +7,9 @@ for dataset in "${datasets[@]}"
 do
   for architecture in "${architectures[@]}"
   do
-    job_name="${architecture}_${dataset}.job"
-    out_file="./logs/${architecture}_${dataset}.out"
-    err_file="./logs/${architecture}_${dataset}.err"
+    job_name="${dataset}_${architecture}.job"
+    out_file="./logs/${dataset}_${architecture}.out"
+    err_file="./logs/${dataset}_${architecture}.err"
     sbatch --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=16G -c 1 -p short -t 0-11:59 batch_jobs/test_NN/single.sh $dataset $architecture
   done
 done
