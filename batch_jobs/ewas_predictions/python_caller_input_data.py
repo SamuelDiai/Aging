c5e3d1d5612e593abe774881b3840b3c6f115a56
Samuel Diai <samuel@MacBook-Pro-de-Samuel.local>
2020-06-12 21:00:28 +0200

    .

--- batch_jobs/ewas_predictions/python_caller_input_data.py
+++ batch_jobs/ewas_predictions/python_caller_input_data.py
@@ -31,29 +31,39 @@ n_cores = int(sys.argv[1])
 features_cols, final_df = load_raw_data(path_raw = '/n/groups/patel/samuel/EWAS/test_inputing.csv', path_output = path_input_env_inputed, path_inputs = path_inputs_env)
 col_age_id_eid_sex_ethnicty = final_df[cols_age_sex_eid_ethnicity]
 ## split continuous and categorical
-split_cols = np.array_split(features_cols, n_cores)
 
 
-def parallel_group_of_features(final_df, split_col):
-    list_features_split = []
-    for col in split_col:
-        column_modified = compute_coefs_and_input(final_df, col)
-        list_features_split.append(column_modified[col])
+# split_cols = np.array_split(features_cols, n_cores)
+# def parallel_group_of_features(final_df, split_col):
+#     list_features_split = []
+#     for col in split_col:
+#         column_modified = compute_coefs_and_input(final_df, col)
+#         list_features_split.append(column_modified[col])
+#
+#     inputed_res = col_age_id_eid_sex_ethnicty
+#     for column in list_features_split:
+#         inputed_res = inputed_res.join(column)
+#     return inputed_res
+#
+# def parallel_group(split_col):
+#     print("Split col : ", split_col)
+#     return parallel_group_of_features(final_df, split_col)
+#
+# pool = Pool(n_cores)
+# final_df_inputed_cols = pool.map(parallel_group, split_cols)
+# pool.close()
+# pool.join()
 
-    inputed_res = col_age_id_eid_sex_ethnicty
-    for column in list_features_split:
-        inputed_res = inputed_res.join(column)
-    return inputed_res
 
-def parallel_group(split_col):
-    print("Split col : ", split_col)
-    return parallel_group_of_features(final_df, split_col)
+def parallel_group(col):
+    print("Col : ", col)
+    column_modified = compute_coefs_and_input(final_df, col)
+    return col_age_id_eid_sex_ethnicty.join(column_modified, how = 'outer')
 
-pool = Pool(n_cores)
-final_df_inputed_cols = pool.map(parallel_group, split_cols)
+pool = Pool(-1)
+final_df_inputed_cols = pool.map(parallel_group, features_cols)
 pool.close()
 pool.join()
-
 
 
 final_df_inputed = col_age_id_eid_sex_ethnicty
