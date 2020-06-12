f698179a11c8dff27a51460b23c9d43be7e28d4c
Samuel Diai <samuel@MacBook-Pro-de-Samuel.local>
2020-06-12 13:42:58 +0200

    .

--- batch_jobs/test_NN/single.sh
+++ batch_jobs/test_NN/single.sh
@@ -8,4 +8,4 @@ module load gcc/6.2.0
 module load python/3.6.0
 module load cuda/10.0
 source /home/sc646/alan_jupytervenv/bin/activate
-python3  batch_jobs/test_NN/python_caller.py $1 $2
+python3  batch_jobs/test_NN/python_caller.py $1 $2 $3
