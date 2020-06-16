6615ca74ee3091bd65bbf3a2f77f40dd39dc0b38
Samuel Diai <samuel@MacBook-Pro-de-Samuel.local>
2020-06-16 22:58:55 +0200

    .

--- aging/model/load_and_save_environment_data.py
+++ aging/model/load_and_save_environment_data.py
@@ -162,7 +162,7 @@ def load_data_env(env_dataset, **kwargs):
     else :
         ## Find columns to read
         path_env = path_inputs_env + env_dataset + '.csv'
-        cols_to_read = pd.read_csv(env_dataset, nrows = 2).set_index('id').columns
+        cols_to_read = pd.read_csv(path_env, nrows = 2).set_index('id').columns
         ## Features + eid + id / without ethnicity + age + sex
         cols_to_read = [elem for elem in cols_to_read if elem not in ['Sex', 'Age when attended assessment centre', 'eid'] + ETHNICITY_COLS] + ['id']
         if use_inputed == True:
