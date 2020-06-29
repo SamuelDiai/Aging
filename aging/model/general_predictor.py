9a53519584223510b0b4913b551b0265c681e036
Samuel Diai <samuel@MacBook-Pro-de-Samuel.local>
2020-06-29 20:23:45 +0200

    .

--- aging/model/general_predictor.py
+++ aging/model/general_predictor.py
@@ -273,7 +273,7 @@ class BaseModel():
             matrix = np.zeros((self.inner_splits, len(columns)))
             for fold, indexes in enumerate(list(cv.split(X.values, y))):
                 train_index, test_index = indexes[0], indexes[1]
-                X_train, X_test, y_train, y_test = X.values[train_index], X.values[test_index], y.values[train_index], y.values[test_index]
+                X_train, X_test, y_train, y_test = X.values[train_index], X.values[test_index], y[train_index], y[test_index]
                 list_corr = [np.abs(stat.pearsonr(X_test[column], y_test)[0]) for column in columns]
                 matrix[fold] = list_corr
             self.features_imp_sd = np.std(matrix, axis = 0)
