f19e958fab44784cde5f86ca72eadac4cc8b9041
Samuel Diai <samuel@macbook-pro-de-samuel.home>
2020-06-29 18:00:07 +0200

    .

--- batch_jobs/predictions/parallel.sh
+++ batch_jobs/predictions/parallel.sh
@@ -2,8 +2,8 @@
 targets=( "Age" "Sex" )
 #targets=( "Sex" )
 models=( "LightGbm" "NeuralNetwork" "ElasticNet" )
-#datasets=( 'HandGripStrength' 'BrainGreyMatterVolumes' 'BrainSubcorticalVolumes' 'HeartSize' 'HeartPWA' 'ECGAtRest' 'AnthropometryImpedance' 'UrineBiochemestry' 'BloodBiochemestry' 'BloodCount' 'EyeAutorefraction' 'EyeAcuity' 'EyeIntraoculaPressure' 'BraindMRIWeightedMeans' 'Spirometry' 'BloodPressure' 'AnthropometryBodySize' 'ArterialStiffness' 'CarotidUltrasound' 'BoneDensitometryOfHeel' 'HearingTest' )
-datasets=( 'BloodPressure' )
+datasets=( 'HandGripStrength' 'BrainGreyMatterVolumes' 'BrainSubcorticalVolumes' 'HeartSize' 'HeartPWA' 'ECGAtRest' 'AnthropometryImpedance' 'UrineBiochemestry' 'BloodBiochemestry' 'BloodCount' 'EyeAutorefraction' 'EyeAcuity' 'EyeIntraoculaPressure' 'BraindMRIWeightedMeans' 'Spirometry' 'BloodPressure' 'AnthropometryBodySize' 'ArterialStiffness' 'CarotidUltrasound' 'BoneDensitometryOfHeel' 'HearingTest' )
+#datasets=( 'BloodPressure' )
 
 outer_splits=10
 inner_splits=9
@@ -78,41 +78,41 @@ do
 		# done
 		for dataset in "${datasets[@]}"
 		do
-			declare -a IDs=()
-			for ((fold=0; fold <= $outer_splits-1; fold++))
-			do
-				job_name="${target}_${model}_${dataset}_${fold}.job"
-				out_file="./logs/${target}_${model}_${dataset}_${fold}.out"
-				err_file="./logs/${target}_${model}_${dataset}_${fold}.err"
-
-				# To del :
-				ID=$(sbatch --parsable  --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 4-23:59 batch_jobs/predictions/single.sh $model $outer_splits $inner_splits $n_iter $target $dataset $fold)
-				IDs+=($ID)
-			done
-
-
-			job_name="${target}_${model}_${dataset}_features.job"
-			out_file="./logs/${target}_${model}_${dataset}_features.out"
-			err_file="./logs/${target}_${model}_${dataset}_features.err"
-
-			# To del :
-
-			sbatch --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 4-23:59 batch_jobs/predictions/single_features.sh $model $n_iter $target $dataset $n_splits
-				#sbatch --error=$err_file --dependency=afterok:$ID_raw --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 4-23:59 batch_jobs/predictions/single_features.sh $model $n_iter $target $dataset $n_splits
-			model="Correlation"
-			job_name="${target}_${model}_${dataset}_features.job"
-			out_file="./logs/${target}_${model}_${dataset}_features.out"
-			err_file="./logs/${target}_${model}_${dataset}_features.err"
-
-			# To del
-			sbatch --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 4-23:59 batch_jobs/predictions/single_features.sh $model $n_iter $target $dataset $n_splits
-
-			job_name="${target}_${model}_${dataset}_postprocessing.job"
-			out_file="./logs/${target}_${model}_${dataset}_postprocessing.out"
-			err_file="./logs/${target}_${model}_${dataset}_postprocessing.err"
-
-			printf -v joinedIDS '%s:' "${IDs[@]}"
-			sbatch --dependency=afterok:${joinedIDS%:} --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p short -t 0-11:59 batch_jobs/predictions/postprocessing.sh $model $target $dataset $outer_splits
+			# declare -a IDs=()
+			# for ((fold=0; fold <= $outer_splits-1; fold++))
+			# do
+			# 	job_name="${target}_${model}_${dataset}_${fold}.job"
+			# 	out_file="./logs/${target}_${model}_${dataset}_${fold}.out"
+			# 	err_file="./logs/${target}_${model}_${dataset}_${fold}.err"
+			#
+			# 	# To del :
+			# 	ID=$(sbatch --parsable  --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 4-23:59 batch_jobs/predictions/single.sh $model $outer_splits $inner_splits $n_iter $target $dataset $fold)
+			# 	IDs+=($ID)
+			# done
+			#
+			#
+			# job_name="${target}_${model}_${dataset}_features.job"
+			# out_file="./logs/${target}_${model}_${dataset}_features.out"
+			# err_file="./logs/${target}_${model}_${dataset}_features.err"
+			#
+			# # To del :
+			#
+			# sbatch --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 4-23:59 batch_jobs/predictions/single_features.sh $model $n_iter $target $dataset $n_splits
+			# 	#sbatch --error=$err_file --dependency=afterok:$ID_raw --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 4-23:59 batch_jobs/predictions/single_features.sh $model $n_iter $target $dataset $n_splits
+			# model="Correlation"
+			# job_name="${target}_${model}_${dataset}_features.job"
+			# out_file="./logs/${target}_${model}_${dataset}_features.out"
+			# err_file="./logs/${target}_${model}_${dataset}_features.err"
+			#
+			# # To del
+			# sbatch --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 4-23:59 batch_jobs/predictions/single_features.sh $model $n_iter $target $dataset $n_splits
+			#
+			# job_name="${target}_${model}_${dataset}_postprocessing.job"
+			# out_file="./logs/${target}_${model}_${dataset}_postprocessing.out"
+			# err_file="./logs/${target}_${model}_${dataset}_postprocessing.err"
+			#
+			# printf -v joinedIDS '%s:' "${IDs[@]}"
+			# sbatch --dependency=afterok:${joinedIDS%:} --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p short -t 0-11:59 batch_jobs/predictions/postprocessing.sh $model $target $dataset $outer_splits
 
 			if [ $target == "Age" ]
 			then
