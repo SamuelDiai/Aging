de6949ce0732bbf3edb12c985014a6519ec947b5
Samuel Diai <samuel@macbook-pro-de-samuel.home>
2020-07-17 11:43:08 +0200

    .

--- aging/model/XgboostEstimators.py
+++ aging/model/XgboostEstimators.py
@@ -13,28 +13,27 @@ class CoxXgboost(BaseEstimator):
 
 
     def fit(self, X, y):
-    """
-    This should fit classifier. All the "work" should be done here.
-
-    Note: assert is not a good choice here and you should rather
-    use try/except blog with exceptions. This is just for short syntax.
-    """
-    #transform y for xgboost formatting
-    y_is_dead, y_time = list(zip(*y))
-    y_is_dead, y_time = np.array(y_is_dead), np.array(y_time)
-    y_time[~y_is_dead] = -y_time[~y_is_dead]
-    y = y_time
-    dtrain = xgb.DMatrix(X, label = y)
-    params = {'objective': 'survival:cox',
-              'colsample_bytree' : self.colsample_bytree,
-              'gamma' : self.gamma,
-              'learning_rate' : self.learning_rate,
-              'max_depth' : self.max_depth,
-              'subsample' : self.subsample}
-    print(params)
-    bst = xgb.train(params, dtrain, num_boost_round=self.n_estimators)
-    self.booster_ = bst
-    return self
+        """
+        This should fit classifier. All the "work" should be done here.
+        Note: assert is not a good choice here and you should rather
+        use try/except blog with exceptions. This is just for short syntax.
+        """
+        #transform y for xgboost formatting
+        y_is_dead, y_time = list(zip(*y))
+        y_is_dead, y_time = np.array(y_is_dead), np.array(y_time)
+        y_time[~y_is_dead] = -y_time[~y_is_dead]
+        y = y_time
+        dtrain = xgb.DMatrix(X, label = y)
+        params = {'objective': 'survival:cox',
+                  'colsample_bytree' : self.colsample_bytree,
+                  'gamma' : self.gamma,
+                  'learning_rate' : self.learning_rate,
+                  'max_depth' : self.max_depth,
+                  'subsample' : self.subsample}
+        print(params)
+        bst = xgb.train(params, dtrain, num_boost_round=self.n_estimators)
+        self.booster_ = bst
+        return self
 
     def predict(self, X):
         try:
