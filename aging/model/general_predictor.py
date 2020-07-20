236fc2fe311e62149280952b9f3d6b6a33779b48
Samuel Diai <samuel@macbook-pro-de-samuel.home>
2020-07-21 00:25:15 +0200

    .

--- aging/model/general_predictor.py
+++ aging/model/general_predictor.py
@@ -294,7 +294,7 @@ class BaseModel():
             ## Compute list_test_folds_eid, and list_train_folds_eid
             if organ in ['Brain', 'Carotids', 'Heart'] or view in ['TrailMaking', 'MatrixPatternCompletion', 'TowerRearranging', 'SymbolDigitSubstitution', 'PairedAssociativeLearning']:
                 list_test_folds = [pd.read_csv(path_eid_split + 'instances23_eids_%s.csv' % fold).columns.astype(int) for fold in range(splits)]
-            elif organ in ['Eyes', 'ECG', 'Vascular', 'Anthropometry', 'Urine', 'BloodB', 'BloodC', 'Lungs', 'Hand', 'Heel', 'BloodPressure', 'Hearing', 'Cognitive'] :
+            elif organ in ['Eyes', 'ECG', 'Vascular', 'Anthropometry', 'Urine', 'BloodB', 'BloodC', 'Lungs', 'Hand', 'Heel', 'BloodPressure', 'Hearing'] :
                 list_test_folds = [pd.read_csv(path_eid_split + '%s_eids_%s.csv' % (organ, fold)).columns.astype(int) for fold in range(splits)]
             elif organ in ['Cognitive'] and view in ['ReactionTime', 'PairsMatching', 'ProspectiveMemory', 'NumericMemory']:
                 list_test_folds = [pd.read_csv(path_eid_split + '%s_%s_eids_%s.csv' % (organ, view, fold)).columns.astype(int) for fold in range(splits)]
