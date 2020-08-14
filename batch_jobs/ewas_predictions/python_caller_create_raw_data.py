ba5003e7387d52a1817da24a79ffba461375c52e
Samuel Diai <samuel@macbook-pro-de-samuel.home>
2020-08-14 02:16:49 +0200

    .

--- batch_jobs/ewas_predictions/python_caller_create_raw_data.py
+++ batch_jobs/ewas_predictions/python_caller_create_raw_data.py
@@ -8,9 +8,19 @@ if sys.platform == 'linux':
 elif sys.platform == 'darwin':
     sys.path.append('/Users/samuel/Desktop/Aging')
 
-from aging.environment_processing.base_processing import path_inputs_env, path_input_env, ETHNICITY_COLS
+from aging.environment_processing.base_processing import path_inputs_env, path_input_env
 from aging.model.load_and_save_environment_data import map_envdataset_to_dataloader_and_field
 
+ETHNICITY_COLS = ['Ethnicity.White',
+       'Ethnicity.British', 'Ethnicity.Irish', 'Ethnicity.White_Other',
+       'Ethnicity.Mixed', 'Ethnicity.White_and_Black_Caribbean',
+       'Ethnicity.White_and_Black_African', 'Ethnicity.White_and_Asian',
+       'Ethnicity.Mixed_Other', 'Ethnicity.Asian', 'Ethnicity.Indian',
+       'Ethnicity.Pakistani', 'Ethnicity.Bangladeshi', 'Ethnicity.Asian_Other',
+       'Ethnicity.Black', 'Ethnicity.Caribbean', 'Ethnicity.African',
+       'Ethnicity.Black_Other', 'Ethnicity.Chinese', 'Ethnicity.Other',
+       'Ethnicity.Other_ethnicity', 'Ethnicity.Do_not_know',
+       'Ethnicity.Prefer_not_to_answer', 'Ethnicity.NA']
 
 df_sex_age_ethnicity = pd.read_csv('/n/groups/patel/Alan/Aging/Medical_Images/data/data-features_instances.csv').set_index('id').drop(columns = ['instance', 'Abdominal_images_quality'])
 df_sex_age_ethnicity = df_sex_age_ethnicity.rename(columns = {'Age' : 'Age when attended assessment centre'})
