1191e5be5da566c4337fcbd4aa283a4045d4f049
Samuel <samuel@macbook-pro-de-samuel.home>
2020-09-18 19:08:58 +0200

    .

--- batch_jobs/ewas_predictions/parallel.sh
+++ batch_jobs/ewas_predictions/parallel.sh
@@ -2,7 +2,7 @@
 #models=( "ElasticNet" )
 models=( "LightGbm" "NeuralNetwork" "ElasticNet" )
 #target_datasets=( "Alcohol" "Anthropometry" "AnthropometryBodySize" "AnthropometryImpedance" "ArterialAndBloodPressure" "ArterialStiffness" "Blood" "BloodBiochemestry" "BloodCount" "BloodPressure" "Brain" "BrainGreyMatterVolumes" "BrainSubcorticalVolumes" "Breathing" "CancerScreening" "ChestPain" "Claudification" "Diet" "ECGAtRest" "Education" "ElectronicDevices" "Employment" "Eye" "EyeAcuity" "EyeAutorefraction" "EyeIntraoculaPressure" "Eyesight" "FamilyHistory" "GeneralHealth" "GeneralPain" "Hearing" "Heart" "HeartPWA" "HeartSize" "Household" "Medication" "MentalHealth" "Mouth" "OtherSociodemographics" "PhysicalActivity" "SexualFactors" "Sleep" "SocialSupport" "SpiroAndArterialAndBp" "Spirometry" "SunExposure" "UrineAndBlood" "UrineBiochemestry" )
-target_datasets=( "*" "*instances01" "*instances1.5x" "*instances23" "Abdomen" "AbdomenLiver" "AbdomenPancreas" "Arterial" "ArterialPulseWaveAnalysis" "ArterialCarotids" "Biochemistry" ) #"BiochemistryUrine" "BiochemistryBlood" "Brain" "BrainCognitive" "BrainMRI" "Eyes" "EyesAll" "EyesFundus" "EyesOCT" "Hearing" "ImmuneSystem" "Lungs" "Musculoskeletal" "MusculoskeletalSpine" "MusculoskeletalHips" "MusculoskeletalKnees" "MusculoskeletalFullBody" "MusculoskeletalScalars" "PhysicalActivity" )
+target_datasets=( "BiochemistryUrine" "BiochemistryBlood" "Brain" "BrainCognitive" "BrainMRI" "Eyes" "EyesAll" "EyesFundus" "EyesOCT" "Hearing" "ImmuneSystem" "Lungs" )#"Musculoskeletal" "MusculoskeletalSpine" "MusculoskeletalHips" "MusculoskeletalKnees" "MusculoskeletalFullBody" "MusculoskeletalScalars" "PhysicalActivity" )
 #target_datasets=( "BrainGreyMatterVolumes" "BrainSubcorticalVolumes" "Brain" "Heart" "HeartSize" "HeartPWA" "AnthropometryImpedance" "UrineBiochemestry" "BloodBiochemestry" "BloodCount" "Blood" "UrineAndBlood" "EyeAutorefraction" "EyeAcuity" "EyeIntraoculaPressure" "Eye" "Spirometry" "BloodPressure" "AnthropometryBodySize" "Anthropometry" "ArterialStiffness" "ArterialAndBloodPressure" "SpiroAndArterialAndBp" )
 #input_datasets=( "Education" )
 input_datasets=( 'Alcohol' 'Diet' 'Education' 'ElectronicDevices' 'Employment' 'FamilyHistory' 'Eyesight' 'Mouth' 'GeneralHealth' 'Breathing' 'Claudification' 'GeneralPain' 'ChestPain' 'CancerScreening' 'Medication' 'Hearing' 'Household' 'MentalHealth' 'OtherSociodemographics' 'PhysicalActivityQuestionnaire' 'SexualFactors' 'Sleep' 'SocialSupport' 'SunExposure' 'EarlyLifeFactors' 'Smoking' )
@@ -130,27 +130,27 @@ for input_dataset in "${input_datasets[@]}"
 
 		for model in "${models[@]}"
 		do
-			# declare -a IDs=()
-			# for ((fold=0; fold <= $outer_splits-1; fold++))
-			# do
-			#   job_name="${target_dataset}_${model}_${input_dataset}_${fold}.job"
-			#   out_file="./logs/${target_dataset}_${model}_${input_dataset}_${fold}.out"
-			#   err_file="./logs/${target_dataset}_${model}_${input_dataset}_${fold}.err"
-			# 	ID=$(sbatch --parsable --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 4-23:59 batch_jobs/ewas_predictions/single.sh $model $outer_splits $inner_splits $n_iter $target_dataset $input_dataset $fold)
-			# 	IDs+=($ID)
-			# done
+			declare -a IDs=()
+			for ((fold=0; fold <= $outer_splits-1; fold++))
+			do
+			  job_name="${target_dataset}_${model}_${input_dataset}_${fold}.job"
+			  out_file="./logs/${target_dataset}_${model}_${input_dataset}_${fold}.out"
+			  err_file="./logs/${target_dataset}_${model}_${input_dataset}_${fold}.err"
+				ID=$(sbatch --parsable --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 4-23:59 batch_jobs/ewas_predictions/single.sh $model $outer_splits $inner_splits $n_iter $target_dataset $input_dataset $fold)
+				IDs+=($ID)
+			done
 
   	  job_name="${target_dataset}_${model}_${input_dataset}_features.job"
   		out_file="./logs/${target_dataset}_${model}_${input_dataset}_features.out"
   		err_file="./logs/${target_dataset}_${model}_${input_dataset}_features.err"
       sbatch --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p medium -t 4-23:59 batch_jobs/ewas_predictions/single_features.sh $model $n_iter $target_dataset $input_dataset $n_splits
 
-			# job_name="${target_dataset}_${model}_${input_dataset}_postprocessing.job"
-			# out_file="./logs/${target_dataset}_${model}_${input_dataset}_postprocessing.out"
-			# err_file="./logs/${target_dataset}_${model}_${input_dataset}_postprocessing.err"
-      #
-			# printf -v joinedIDS '%s:' "${IDs[@]}"
-			# sbatch --dependency=afterok:${joinedIDS%:} --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p short -t 0-11:59 batch_jobs/ewas_predictions/postprocessing.sh $model $target_dataset $input_dataset $outer_splits
+			job_name="${target_dataset}_${model}_${input_dataset}_postprocessing.job"
+			out_file="./logs/${target_dataset}_${model}_${input_dataset}_postprocessing.out"
+			err_file="./logs/${target_dataset}_${model}_${input_dataset}_postprocessing.err"
+
+			printf -v joinedIDS '%s:' "${IDs[@]}"
+			sbatch --dependency=afterok:${joinedIDS%:} --error=$err_file --output=$out_file --job-name=$job_name --mem-per-cpu=$memory -c $n_cores -p short -t 0-11:59 batch_jobs/ewas_predictions/postprocessing.sh $model $target_dataset $input_dataset $outer_splits
 		done
 	done
 done
