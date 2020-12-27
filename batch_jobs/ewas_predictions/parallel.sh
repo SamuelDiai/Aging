c7749a8c899f6f86e2bf3f6dcc2279340ef24384
Samuel <samuel@macbook-pro-de-samuel.home>
2020-12-27 17:39:12 +0100

    PA

--- batch_jobs/ewas_predictions/parallel.sh
+++ batch_jobs/ewas_predictions/parallel.sh
@@ -9,7 +9,7 @@ target_datasets=( '*instances01' '*instances1.5x' '*instances23' 'Abdomen' 'Abdo
 #  'PhysicalActivity' )
 
 #input_datasets=( 'Alcohol' 'Diet' 'Education' 'ElectronicDevices' 'Employment' 'FamilyHistory' 'Eyesight' 'Mouth' 'GeneralHealth' 'Breathing' 'Claudification' 'GeneralPain' 'ChestPain' 'CancerScreening' 'Medication' 'Hearing' 'Household' 'MentalHealth' 'OtherSociodemographics' 'PhysicalActivityQuestionnaire' 'SexualFactors' 'Sleep' 'SocialSupport' 'SunExposure' 'EarlyLifeFactors' 'Smoking' )
-input_datasets=( 'Clusters_CognitiveTrailMaking' 'Clusters_ECGAtRest' 'Clusters_EyeAcuity' 'Clusters_EyeAutorefraction' 'Clusters_EyeIntraocularPressure' 'Clusters_VascularAllBiomarkers' 'Clusters_UrineBiochemistry' 'Clusters_Spirometry' )
+input_datasets=( 'Clusters_PhysicalActivity' )
 #'ArterialStiffness' 'Biochemistry' 'BloodBiochemistry' 'BloodCount' 'BloodPressure' 'BoneDensitometryOfHeel' 'BrainAndCognitive' 'BraindMRIWeightedMeans' 'BrainGreyMatterVolumes' 'BrainMRIAllBiomarkers' 'BrainSubcorticalVolumes' 'CarotidUltrasound' 'CognitiveAllBiomarkers' 'CognitiveFluidIntelligence' 'CognitiveMatrixPatternCompletion' 'CognitiveNumericMemory' 'CognitivePairedAssociativeLearning' 'CognitivePairsMatching' 'CognitiveProspectiveMemory' 'CognitiveReactionTime' 'CognitiveSymbolDigitSubstitution' 'CognitiveTowerRearranging' 'CognitiveTrailMaking' 'ECGAtRest' 'EyeAcuity' 'EyeAutorefraction' 'EyeIntraocularPressure' 'VascularAllBiomarkers' 'UrineBiochemistry' 'Spirometry' 'PhysicalActivity' 'MusculoskeletalAllBiomarkers' 'HeartSize' 'HeartPWA' 'EyesAllBiomarkers' 'HandGripStrength' 'HearingTest' 'HeartAllBiomarkers' 'HeartMRIAll' )
 
 
@@ -20,7 +20,7 @@ inner_splits=9
 n_iter=30
 n_splits=10
 
-memory=8G
+memory=20G
 n_cores=1
 
 # declare -a IDsLoads=()
