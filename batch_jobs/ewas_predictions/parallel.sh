bc36efb8fd9f7e9f82f0cea4db52a8d1ee66836a
Samuel <samuel@macbook-pro-de-samuel.home>
2021-02-11 19:44:31 +0100

    .

--- batch_jobs/ewas_predictions/parallel.sh
+++ batch_jobs/ewas_predictions/parallel.sh
@@ -9,7 +9,7 @@ target_datasets=( '*instances01' '*instances1.5x' '*instances23' 'Abdomen' 'Abdo
 #  'PhysicalActivity' )
 
 #input_datasets=( 'Alcohol' 'Diet' 'Education' 'ElectronicDevices' 'Employment' 'FamilyHistory' 'Eyesight' 'Mouth' 'GeneralHealth' 'Breathing' 'Claudification' 'GeneralPain' 'ChestPain' 'CancerScreening' 'Medication' 'Hearing' 'Household' 'MentalHealth' 'OtherSociodemographics' 'PhysicalActivityQuestionnaire' 'SexualFactors' 'Sleep' 'SocialSupport' 'SunExposure' 'EarlyLifeFactors' 'Smoking' )
-input_datasets=( 'Clusters_MusculoskeletalAllBiomarkers' 'Clusters_HeartSize' 'Clusters_HeartPWA' 'Clusters_EyesAllBiomarkers' 'Clusters_HandGripStrength' 'Clusters_HearingTest' 'Clusters_HeartAllBiomarkers' 'Clusters_HeartMRIAll' )
+input_datasets=( 'Clusters_ENSEMBLE_HealthAndMedicalHistory' 'Clusters_ENSEMBLE_LifestyleAndEnvironment' 'Clusters_ENSEMBLE_PsychosocialFactors' 'Clusters_ENSEMBLE_SocioDemographics' )
 #'ArterialStiffness' 'Biochemistry' 'BloodBiochemistry' 'BloodCount' 'BloodPressure' 'BoneDensitometryOfHeel' 'BrainAndCognitive' 'BraindMRIWeightedMeans' 'BrainGreyMatterVolumes' 'BrainMRIAllBiomarkers' 'BrainSubcorticalVolumes' 'CarotidUltrasound' 'CognitiveAllBiomarkers' 'CognitiveFluidIntelligence' 'CognitiveMatrixPatternCompletion' 'CognitiveNumericMemory' 'CognitivePairedAssociativeLearning' 'CognitivePairsMatching' 'CognitiveProspectiveMemory' 'CognitiveReactionTime' 'CognitiveSymbolDigitSubstitution' 'CognitiveTowerRearranging' 'CognitiveTrailMaking' 'ECGAtRest' 'EyeAcuity' 'EyeAutorefraction' 'EyeIntraocularPressure' 'VascularAllBiomarkers' 'UrineBiochemistry' 'Spirometry' 'PhysicalActivity' 'MusculoskeletalAllBiomarkers' 'HeartSize' 'HeartPWA' 'EyesAllBiomarkers' 'HandGripStrength' 'HearingTest' 'HeartAllBiomarkers' 'HeartMRIAll' )
 
 
