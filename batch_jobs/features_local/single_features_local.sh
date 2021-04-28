349373128ce0703acdf07df3a40b8921d67edfc6
Samuel <samuel@macbook-pro-de-samuel.home>
2021-04-28 15:35:55 +0200

    .

--- batch_jobs/features_local/single_features_local.sh
+++
@@ -1,3 +0,0 @@
-
-source ~/hms/bin/activate
-python3  batch_jobs/features_local/python_caller_features_local.py $1 $2 $3 $4 $5
--- batch_jobs/predictions_local/single_local.sh
+++
@@ -1,3 +0,0 @@
-
-source ~/hms/bin/activate
-python3  batch_jobs/predictions_local/python_caller_local.py $1 $2 $3 $4 $5 $6 $7
