5ae81dce7dbd0276822b63c369538a188e332030
Samuel <samuel@macbook-pro-de-samuel.home>
2021-05-07 18:17:57 +0200

    .

--- aging/environment_processing/base_processing.py
+++ aging/environment_processing/base_processing.py
@@ -40,29 +40,29 @@ def read_data(cols_categorical, cols_features, instance, **kwargs):
 	cols_features : contains the id of the continuous (non categorical) features.
 	instance : select the UK-biobank instance.
 	"""
-    nrows = None
-    if 'nrows' in kwargs.keys():
-        nrows = kwargs['nrows']
+	nrows = None
+	if 'nrows' in kwargs.keys():
+		nrows = kwargs['nrows']
 
 
-    df_features = pd.read_csv(path_dictionary, usecols = ["FieldID", "Field"])
-    df_features.set_index('FieldID', inplace = True)
-    feature_id_to_name = df_features.to_dict()['Field']
+	df_features = pd.read_csv(path_dictionary, usecols = ["FieldID", "Field"])
+	df_features.set_index('FieldID', inplace = True)
+	feature_id_to_name = df_features.to_dict()['Field']
 
-    #age_col = '21003-' + str(instance) + '.0'
-    #print("PATH DATA : ", path_data)
-    temp = pd.read_csv(path_data, usecols = ['eid'] + cols_features + cols_categorical, nrows = nrows)
-    for col in cols_categorical:
-        temp[col] = temp[col].astype('Int64')
-    temp.set_index('eid', inplace = True)
+	#age_col = '21003-' + str(instance) + '.0'
+	#print("PATH DATA : ", path_data)
+	temp = pd.read_csv(path_data, usecols = ['eid'] + cols_features + cols_categorical, nrows = nrows)
+	for col in cols_categorical:
+		temp[col] = temp[col].astype('Int64')
+	temp.set_index('eid', inplace = True)
 
-    features_index = temp.columns
-    features = []
-    for elem in features_index:
-        features.append(feature_id_to_name[int(elem.split('-')[0])] + elem.split('-')[1][-2:])
-    df = temp.dropna(how = 'any')
-    df.columns = features
-    return df
+	features_index = temp.columns
+	features = []
+	for elem in features_index:
+		features.append(feature_id_to_name[int(elem.split('-')[0])] + elem.split('-')[1][-2:])
+	df = temp.dropna(how = 'any')
+	df.columns = features
+	return df
 
 
 def read_complex_data(instances, dict_onehot, cols_numb_onehot, cols_ordinal_, cols_continuous_, cont_fill_na_, cols_half_binary_ = [], **kwargs):
